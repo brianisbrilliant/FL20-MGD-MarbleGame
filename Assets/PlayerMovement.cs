695502cccb176c0a0ff6516c81c81f3de1f5f0c6
Brian Foster <brianisbrilliant@gmail.com>
2020-10-07 08:55:11 -0500

    committing from the cmd

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // asdf
     }
 
     // Update is called once per frame
