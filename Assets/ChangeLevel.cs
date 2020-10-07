6862aaadee5d15e5779cd02cef8f7d261cef97cc
Brian Foster <brianisbrilliant@gmail.com>
2020-10-07 10:33:43 -0500

    Built movement, resetplayer, changing scenes.

---
+++ Assets/ChangeLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevel : MonoBehaviour
+{
+	[Tooltip("The name of the scene you want to go to.")]
+	public string destination;
+
+    void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Player")) {
+			if(destination == "") destination = "MainMenu";
+			UnityEngine.SceneManagement.SceneManager.LoadScene(destination);
+		}
+	}
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        // asdf
-    }
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool phoneIsConnected = false;
+
+	[HideInInspector]
+	public Vector3 dir, startPosition;
+	Rigidbody rb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = this.GetComponent<Rigidbody>();
+		startPosition = this.transform.position;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.R)) ResetPlayer();
+		if(this.transform.position.y < -2) ResetPlayer();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		dir = Vector3.zero;
+        if(phoneIsConnected) {
+			dir.x = Input.acceleration.x;
+			dir.z = Input.acceleration.y;
+		} else {
+			dir.x = Input.GetAxis("Horizontal");
+			dir.z = Input.GetAxis("Vertical");
+		}
+
+		rb.AddForce(dir * 10);
+	}
+
+	public void ResetPlayer() {
+		rb.velocity = Vector3.zero;
+		this.transform.position = startPosition;
+	}
 }
