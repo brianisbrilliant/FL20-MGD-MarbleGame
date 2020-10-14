14aac7d84fa582d8c5905eea0ef36ed76893115c
Brian Foster <brianisbrilliant@gmail.com>
2020-10-14 10:30:01 -0500

    Added force platforms and coins

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+// add coin pickup sound
+// add scoretext
+// add coin material
+// add jump powerup
 
 public class PlayerMovement : MonoBehaviour
-	{
+{
 
 	public bool phoneIsConnected = false;
 
+	[Header("Movement")]
+	public float jumpForce = 5;
+
 	[Header("Audio Clips")]
 	public AudioClip calibrateClip;
+	public AudioClip jumpClip;
+	public AudioClip coinClip;
+
+	[Header("Score UI")]
+	public TextMeshProUGUI scoreText;
 
 	[HideInInspector]
 	public Vector3 dir, startPosition, calibratedDir;
@@ -16,6 +30,11 @@ public class PlayerMovement : MonoBehaviour
 	// private variables
 	Rigidbody rb;
 	AudioSource aud;
+	bool isGrounded = true;
+	bool canJump = false;
+
+	int score = 0;
+	int coinScore = 250;
 
 	// Start is called before the first frame update
 	void Start()
@@ -30,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
 	void Update(){
 		if(Input.GetKeyDown(KeyCode.R)) ResetPlayer();
 		if(this.transform.position.y < -2) ResetPlayer();
+		if(!phoneIsConnected && Input.GetKeyDown(KeyCode.Space)) Jump();
 	}
 
 	// Update is called once per frame
@@ -60,6 +80,33 @@ public class PlayerMovement : MonoBehaviour
 
 		aud.PlayOneShot(calibrateClip);
 	}
+
+	public void Jump() {
+		if(isGrounded && canJump) {
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+			aud.PlayOneShot(jumpClip);
+		}
+	}
+
+	void OnCollisionEnter() { isGrounded = true; }
+	void OnCollisionExit() { isGrounded = false; }
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("Coin")) {
+			score += coinScore;
+			scoreText.text = "Score = " + score;
+			aud.PlayOneShot(coinClip);
+			Destroy(other.gameObject);
+		}
+		if(other.gameObject.name == "Jump Pickup") {
+			canJump = true;
+			aud.PlayOneShot(jumpClip);
+			Destroy(other.gameObject);
+		}
+	}
+
 }
 
 
@@ -70,10 +117,10 @@ public class PlayerMovement : MonoBehaviour
 
 
 /*
-	Calibrate Tilt function
-	Jump function
+	Calibrate Tilt function - DONE
+	Jump function - DONE
 	Force Platforms
 	score and pickups
-	Player don't destroy on load
+	Player don't destroy on load - DONE
 
 */
