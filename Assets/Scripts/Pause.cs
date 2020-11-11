16f398ede37865d66a4a85f475413eaeba86f6f4
Brian Foster <brianisbrilliant@gmail.com>
2020-11-11 09:14:07 -0600

    Added Pause and models to testScene

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+	bool isPaused = false;
+
+	public GameObject pausePanel;
+
+	void Start() {
+		if(pausePanel == null){
+			Debug.Log("Looking for PausePanel...");
+			pausePanel = GameObject.Find("PauseMenu");
+		}
+		pausePanel.SetActive(false);
+		Debug.Log("isPaused = " + isPaused);
+	}
+
+	public void PauseGame() {
+
+		isPaused = !isPaused;		// flip the boolean
+
+		Debug.Log("The PauseGame() method has been called. isPaused: " + isPaused);
+		if(isPaused) {
+			pausePanel.SetActive(true);			// show the pause menu
+			Time.timeScale = 0;					// freeze time
+		} else {
+			pausePanel.SetActive(false);		// hide the pause menu
+			Time.timeScale = 1;					// set time back to normal
+		}
+
+		
+	}
+}
